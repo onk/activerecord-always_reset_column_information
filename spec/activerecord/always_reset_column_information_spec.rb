f99c539c67bd8ee35a1a009c4ff7ef20fc8255c4
Takafumi ONAKA <takafumi.onaka@gmail.com>
2017-10-18 16:55:55 +0900

    Configure rspec

--- spec/activerecord/always_reset_column_information_spec.rb
+++ spec/activerecord/always_reset_column_information_spec.rb
@@ -1,11 +1,2 @@
-require "spec_helper"
-
 RSpec.describe Activerecord::AlwaysResetColumnInformation do
-  it "has a version number" do
-    expect(Activerecord::AlwaysResetColumnInformation::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
